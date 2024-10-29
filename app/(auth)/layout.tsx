82179ffae57c77b5d8904a7f954269f6cf29a490
unknown <shashankbud80@gmail.com>
2024-10-29 15:14:58 +0530

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function layout ({ children }:{
+    children:React.ReactNode
+}) {
+  return <div>
+
+    <div>hey 40% offf </div>
+    {children}
+  </div>
+  
+}
+
